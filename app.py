0724e5de6220ef57d18d6894f8cec9eaf2130fe7
Mahmoud Fathy Abukilala <92957303+mahmoud35634@users.noreply.github.com>
2025-08-05 21:07:34 +0300

    t

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Sanad Analytics", layout="centered")
-# col1, col2, col3 = st.columns([1, 2, 1])
-# with col2:
-#     st.image("logo.png", width=200)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("logo.png", width=200)
 st.title("📊 Sanad Analytics Toolkit")
 st.subheader("Welcome to the Sanad Analytics Toolkit! Choose a tool from the sidebar to get started.")
 st.markdown(
