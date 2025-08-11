12e55c3532f67342898405b1fa3f59bc6a18f707
Mahmoud Fathy Abukilala <92957303+mahmoud35634@users.noreply.github.com>
2025-08-11 09:25:46 +0300

    s

--- pages/salesman_dashboard.py
+++ pages/salesman_dashboard.py
@@ -37,7 +37,7 @@ def connect_to_sheet():
         scopes=scopes
     )
     client = gspread.authorize(creds)
-    sheet_id = "16IxEZH4goUOiRloFhYayGhRO-K6t_aXMl8nscHdPzhc"
+    sheet_id = "10YX-uBpIsD62rcUFOFtzVRcc0Bpz404V"
     workbook = client.open_by_key(sheet_id)
     sheet = workbook.get_worksheet(2)  # index 2 = 3rd sheet
     return sheet
