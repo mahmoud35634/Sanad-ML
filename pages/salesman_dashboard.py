5e8def90758c56ccc3fb172a49ed29449c092de3
Mahmoud Fathy Abukilala <92957303+mahmoud35634@users.noreply.github.com>
2025-08-12 15:01:30 +0300

    s

--- pages/salesman_dashboard.py
+++ pages/salesman_dashboard.py
@@ -12,7 +12,7 @@ import json
 
 @st.cache_resource
 def load_content_model():
-    with open("content_model_sub.pkl", "rb") as f:
+    with open("content_model.pkl", "rb") as f:
         model_data = pickle.load(f)
     return model_data
 
