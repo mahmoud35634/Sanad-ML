cafa7ff6c043996d96eba0b71dc59b6f0b1c7ad0
Mahmoud Fathy Abukilala <92957303+mahmoud35634@users.noreply.github.com>
2025-08-24 21:46:31 +0300

    s

--- pages/salesman_dashboard.py
+++ pages/salesman_dashboard.py
@@ -377,7 +377,7 @@ def get_two_months_ago_data(sanad_id):
         LEFT JOIN MP_Customers c ON s.CustomerID = c.SITE_NUMBER
         LEFT JOIN MP_Items i ON s.ItemId = i.ITEM_CODE
         WHERE 
-            s.Date >= DATEADD(MONTH, -2, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1))
+            s.Date >= DATEADD(MONTH, -3, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1))
             AND s.Date < DATEADD(MONTH, -1, DATEFROMPARTS(YEAR(GETDATE()), MONTH(GETDATE()), 1))
             AND c.CUSTOMER_B2B_ID = '{sanad_id}'
             AND i.ITEM_CODE NOT LIKE '%XE%'
